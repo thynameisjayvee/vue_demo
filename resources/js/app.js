4d6d4e04559006712551767b93f41bcff8cc04f8
jayvee <jayveetolibas@gmail.com>
2018-12-11 17:06:25 +0800

    Vue Demo part 5, stop at App.vue right side bar, resume at Board.vue

---
+++ resources/js/app.js
@@ -0,0 +1,69 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+import VueRouter from 'vue-router'
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+Vue.use(VueRouter)
+
+import App from './views/App'
+import Dashboard from './views/Board'
+import Login from './views/Login'
+import Register from './views/Register'
+import Home from './views/Welcome'
+
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/board',
+      name: 'board',
+      component: Dashboard,
+    },
+  ],
+});
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+    components: { App },
+    router,
+});
---
+++ tests/Unit/APITest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class APITest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testUserCreation()
+        {
+            $response = $this->json('POST', '/api/register', [
+                'name' => 'Demo User',
+                'email' => str_random(10) . '@demo.com',
+                'password' => '12345',
+            ]);
+
+            $response->assertStatus(200)->assertJsonStructure([
+                'success' => ['token', 'name']
+            ]);
+        }
+
+    public function testUserLogin()
+    {
+        $response = $this->json('POST', '/api/login', [
+            'email' => 'demo@demo.com',
+            'password' => 'secret'
+        ]);
+
+        $response->assertStatus(200)->assertJsonStructure([
+            'success' => ['token']
+        ]);
+    }
+
+    public function testCategoryFetch()
+    {
+        $user = \App\User::find(1);
+
+        $response = $this->actingAs($user, 'api')
+            ->json('GET', '/api/category')
+            ->assertStatus(200)->assertJsonStructure([
+                '*' => [
+                    'id',
+                    'name',
+                    'created_at',
+                    'updated_at',
+                    'deleted_at'
+                ]
+            ]
+        );
+    }
+}
